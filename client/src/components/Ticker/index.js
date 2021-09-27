cf6f533886c59b4d0ab18f6275cf52f64b99b503
eccentricality <youngmin13@gmail.com>
2021-09-27 17:04:39 -0400

    added fetch request for basic seach by name and appended

---
+++ client/src/components/Ticker/index.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+
+export default function Ticker() {
+  const [stocks, setStocks] = useState([]);
+  const apiKey = "bqrZxpYkw4h0AygR9ctu3oqS3YWvgk5O";
+
+  // use async/await to fetch data from
+  useEffect(() => {
+    getData();
+
+    async function getData() {
+      const res = await fetch(
+        `https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&sort=ticker&order=asc&limit=10&apiKey=${apiKey}`
+      );
+      const data = await res.json();
+      // store data into variables
+      setStocks(data.results);
+    }
+  }, []);
+
+  if (!stocks) {
+    return null;
+  }
+  
+  return (
+    <div>
+      <h1>STOCKS</h1>
+      {/* display data from API */}
+      {stocks && (
+        <div className="stocks">
+          {/* loop over the stocks */}
+          {stocks.map((stock, index) => (
+            <div key={index}>
+              <h2>{stock.name}</h2>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
