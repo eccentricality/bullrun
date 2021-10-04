88929d54f7bad2c6fde3e5647aefdccbc11ab29a
Brett Markland-Sanchez <Brett.MarklandSanchez@gmail.com>
2021-10-04 11:24:16 -0400

    refined homepage code

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,7 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
-import Auth from '../utils/auth';
-import { QUERY_USERS, QUERY_GOOGLE_TRENDS } from '../utils/queries';
-import Header from '../components/Header';
-import Ticker from '../components/Ticker';
-import Sidenav from '../components/Sidenav';
 import Banner from '../components/Banner';
-import Footer from '../components/Footer';
 
 const Home = () => {
-
-  const renderUsername = () => {
-    if (!Auth.loggedIn()) return null;
-    return Auth.getProfile().data.username;
-  }
   return (
     <div>
       <Banner/>
